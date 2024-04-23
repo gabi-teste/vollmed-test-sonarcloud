b48e64ae61e33ff20c46163a7f0c9daae1542d77
gabi-teste <167811546+gabi-teste@users.noreply.github.com>
2024-04-23 14:25:19 -0300

    Update Paciente.java

--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -39,16 +39,15 @@ public class Paciente {
     }
 
     public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
-        if (dados.nome() != null) {
-            this.nome = dados.nome();
-        }
         if (dados.telefone() != null) {
             this.telefone = dados.telefone();
         }
         if (dados.endereco() != null) {
             this.endereco.atualizarInformacoes(dados.endereco());
         }
-
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
     }
 
     public void excluir() {
